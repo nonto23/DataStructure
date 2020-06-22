85b7b3ffc716bbfd2e7f8a60d999ddc96e6528df
Ntosh <nonto23>
2020-06-22 10:48:50 +0200

    updated project

---
+++ src/test/java/GameLiveTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameOfLifeTest {
+    @Test
+    public void positionsShouldBeEqualByValue() {
+    }
+
+}
